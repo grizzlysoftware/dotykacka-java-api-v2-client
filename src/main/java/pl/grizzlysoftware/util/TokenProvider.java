a098bb7544f3db2927765c0eaada32810bc50c2e
bpawlowski <bpawlowski@grizzlysoftware.pl>
2021-11-25 23:01:43 +0100

    chore(docs): updated license

--- src/main/java/pl/grizzlysoftware/util/TokenProvider.java
+++ src/main/java/pl/grizzlysoftware/util/TokenProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 Grizzly Software, https://grizzlysoftware.pl
+ * Copyright (c) 2021 Grizzly Software, https://grizzlysoftware.pl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
  * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
@@ -14,6 +14,7 @@
  * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ *
  */
 
 package pl.grizzlysoftware.util;
